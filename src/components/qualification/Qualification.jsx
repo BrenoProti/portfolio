1bb8ad420bf26cfc2de74d98a4196d2ba50e0105
= <moreirasgbr@gmail.com>
2023-06-20 20:38:52 -0300

    qualifications

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './qualification.css'
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+        <h2 className="section__title">
+            Qualificação
+        </h2>
+
+        <span>
+            Minha jornada
+        </span>
+    </section>
+  )
+}
+
+export default Qualification
